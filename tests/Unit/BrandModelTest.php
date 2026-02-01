60f9d297bd4000f88b8a7a3fa3839177a24378a0
Didarul Alam Rahat <darahat42@gmail.com>
2026-02-02 00:32:17 +0600

    Testing test functions

---
+++ tests/Unit/BrandModelTest.php
@@ -0,0 +1,16 @@
+<?php
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use App\Models\Brand;
+use App\Models\Category;
+use App\Models\Product;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class BrandModelTest extends TestCase{
+    use RefreshDatabase;
+
+    public function test_brand_has_correct_fillable_attributes(){
+        // TODO: YOu will write this test in the next
+    }
+}
