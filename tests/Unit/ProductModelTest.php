e9c88a124abb09175e9ef114bec754af03bbfff5
Didarul Alam Rahat <darahat42@gmail.com>
2026-02-24 01:01:54 +0600

    product model test done

--- tests/Unit/ProductModelTest.php
+++ tests/Unit/ProductModelTest.php
@@ -9,6 +9,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Mail;
 
 class ProductModelTest extends TestCase{
+    use RefreshDatabase;
     public function test_product_has_correct_fillable_attributes(){
     /// Arrange
     $product = new Product;
@@ -33,6 +34,27 @@ class ProductModelTest extends TestCase{
         'is_todays_deal',
         'rating'], $fillable
     );
-
     }
+    function test_products_belongs_to_a_category(){
+        /// Arrange
+        $category = Category::factory()->create();
+        $product = Product::factory()->create([
+            'category_id' => $category->id
+        ]);
+        // Check relationship
+
+        $this->assertInstanceOf(Category::class,$product->category);
+        $this->assertTrue($product->category->is($category));
+     }
+     function test_product_belongs_to_brand(){
+        // Arrange
+        $brand = Brand::factory()->create();
+        $product = Product::factory()->create([
+            'brand_id' => $brand->id,
+        ]);
+
+        $this->assertInstanceOf(Brand::class,$product->brand);
+        $this->assertTrue($product->brand->is($brand));
+
+     }
 }
